9e106db6e9673d8310c0b39b8800393b97731407
koopeek <slawekkopaczewski@gmail.com>
2020-09-01 19:38:12 +0200

    Add input and select components

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { SelectField } from "../SelectField/SelectField";
+import { InputField } from "../InputField/InputField";
+import { diets, intolerances, mealTypes } from "../../constans";
 import styles from "./SearchBox.module.scss";
 
 const SearchBox: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [diet, setDiet] = useState<string>("");
+  const [intolerance, setIntolerance] = useState<string>("");
+  const [mealType, setMealType] = useState<string>("");
+
   return (
     <div className={styles.container}>
-      <span>Search box</span>
+      <div className={styles.nameField}>
+        <InputField handleChange={setName} placeholder="Enter meal name" />
+      </div>
+      <div className={styles.selectFields}>
+        <SelectField options={mealTypes} handleChange={setMealType} />
+        <SelectField options={diets} handleChange={setDiet} />
+        <SelectField options={intolerances} handleChange={setIntolerance} />
+      </div>
+      <button className={styles.buttonSearch}>
+        <div>Find meal</div>
+      </button>
     </div>
   );
 };
---
+++ src/constans.ts
@@ -0,0 +1,43 @@
+export const diets: string[] = [
+  "gluten free",
+  "ketogenic",
+  "vegetarian",
+  "lacto vegetarian",
+  "ovo vegetarian",
+  "vegan",
+  "pascetarian",
+  "paleo",
+  "primal",
+  "whole30",
+];
+
+export const intolerances: string[] = [
+  "diary",
+  "egg",
+  "gluten",
+  "grain",
+  "peanut",
+  "seafood",
+  "sesame",
+  "shellfish",
+  "soy",
+  "sulfite",
+  "wheat",
+];
+
+export const mealTypes: string[] = [
+  "main course",
+  "side dish",
+  "dessert",
+  "appetizer",
+  "salad",
+  "bread",
+  "breakfast",
+  "soup",
+  "beverage",
+  "sauce",
+  "marinade",
+  "fingerfood",
+  "snack",
+  "drink",
+];
