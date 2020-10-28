b91211695320c81445ce4776d0a0e71dd567919f
koopeek <slawekkopaczewski@gmail.com>
2020-10-28 18:46:31 +0100

    Add prettier as dev dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import { Recipe } from "./types/types";
-import {Header} from "./components/Header/Header";
+import { Header } from "./components/Header/Header";
 import { Content } from "./components/Content/Content";
 import "./App.scss";
 
 const App: React.FC = () => {
-
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [currentStep, setCurrentStep] = useState<number>(1);
 
   return (
-      <div className="app">
-          <Header />
-          <main className="app__content">
-              <Content recipes={recipes} setCurrentStep={setCurrentStep} currentStep={currentStep} setRecipes={setRecipes} />
-          </main>
-      </div>
+    <div className="app">
+      <Header />
+      <main className="app__content">
+        <Content
+          recipes={recipes}
+          setCurrentStep={setCurrentStep}
+          currentStep={currentStep}
+          setRecipes={setRecipes}
+        />
+      </main>
+    </div>
   );
 };
 
